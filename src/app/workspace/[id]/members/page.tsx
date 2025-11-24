ff2eb81dde2369ccbdc526cd40883b8cdf31a3ce
jasmin <cjm98@smartel.co.kr>
2025-11-24 17:53:04 +0900

    feat : workspace member UI CRUD

--- src/app/workspace/[id]/members/page.tsx
+++ src/app/workspace/[id]/members/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
-
-const Page = () => {
+import MemberForm from "@/app/workspace/[id]/members/memberForm";
+export default function MembersPage() {
     return (
-        <div>
-            맴버 리스트 페이지
+        <div className={'bg-[#1b2026] px-3 py-2.5 rounded-xl'}>
+            <h1 className="text-2xl font-bold">멤버 관리</h1>
+            <p className="mt-1 text-sm text-gray-500">
+                워크스페이스 멤버를 초대하고 권한을 관리하세요.
+            </p>
+            <MemberForm/>
         </div>
     );
-};
-
-export default Page;
+}
